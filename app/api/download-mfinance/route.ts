5e4ccb0f355af395e028f315833697db0c7f4617
Avigdor Meir <avmeir@gmail.com>
2026-09-08 19:22:50 +0300

    release v69.88.0

--- app/api/download-mfinance/route.ts
+++ app/api/download-mfinance/route.ts
@@ -1,11 +1,12 @@
-import { NextResponse } from 'next/server'
+﻿import { NextResponse } from 'next/server'
 
 export const dynamic = 'force-dynamic'
 
-const EXE_URL = 'https://github.com/avigdor12/KeyClick/releases/download/v69.87.1/M_Finance-win-Setup.exe'
+const EXE_URL = 'https://github.com/avigdor12/KeyClick/releases/download/v69.88.0/M_Finance-win-Setup.exe'
 
 // מפנים את הדפדפן ישירות לקובץ ב-GitHub במקום להזרים 139MB דרך הפונקציה.
 // כך אין תעבורת Fast Origin Transfer דרך Vercel לכל הורדה.
 export async function GET() {
   return NextResponse.redirect(EXE_URL, 302)
 }
+
